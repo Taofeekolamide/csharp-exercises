c7bd8cd08840e6610dc7ce7e4bd8b237d5a8964c
Taofeekolamide <taofeekolamide55@gmail.com>
2024-12-08 19:30:43 +0100

    csharp exercises

--- SecondChapter/obj/Debug/net6.0/SecondChapter.AssemblyInfo.cs
+++ SecondChapter/obj/Debug/net6.0/SecondChapter.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SecondChapter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+556f1727390ca237b3be669ed04b5223ecdc0549")]
 [assembly: System.Reflection.AssemblyProductAttribute("SecondChapter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SecondChapter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
